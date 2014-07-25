06900799213c8ac5ff4f502be08f74f57b6ab8ed
srijan198 <shreejan@gmail.com>
2014-07-24 20:44:27 -0400

    Sajha_Bato
    OpenGTS based vehicle tracking system

---
+++ src/org/opengts/CompileTime.java
@@ -0,0 +1,10 @@
+package org.opengts;
+public class CompileTime
+{
+    // 2014/07/22 20:36:07 EDT
+    public static final long   COMPILE_TIMESTAMP    = 1406075767L;
+    public static final String COMPILE_DATETIME     = "2014/07/22 20:36:07 EDT";
+    public static final String SERVICE_ACCOUNT_ID   = "opengts";
+    public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
+    public static final String SERVICE_ACCOUNT_KEY  = "";
+}
